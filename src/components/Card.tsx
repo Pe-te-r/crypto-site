a3f00c3c68264a42bf2e137e067852eaae7d4a3b
Pe-te-r <mburupeter8526@gmail.com>
2024-10-05 16:22:04 +0300

    why choose us end

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Card = ({ title, description }: any) => {
+const Card = ({ image, title, description, side }) => {
+  // Determine the order based on the 'side' prop
+  const isImageLeft = side === 'left';
+
   return (
-    <div className="bg-[#F7E94D] p-4 rounded-lg shadow-md w-full md:w-1/2 lg:w-1/4 m-4">
-      <h5 className="text-center font-semibold text-lg">{title}</h5>
-      <p className="font-serif p-2 font-normal">{description}</p>
+    <div className="flex flex-col max-h-fit md:flex-row items-center m-2 justify-center p-6 bg-white rounded-lg shadow-lg mb-6 w-full max-w-[500px] md:max-w-[750px]">
+      {/* Conditional rendering of the image and text based on side prop */}
+      <div className={`w-full md:w-1/2 ${isImageLeft ? 'order-1' : 'order-2'} p-4`}>
+        <img src={image} alt={title} className="w-full h-auto rounded-lg object-cover" />
+      </div>
+      <div className={`w-full md:w-1/2 ${isImageLeft ? 'order-2' : 'order-1'} p-4 text-center flex flex-col justify-around h-full`}>
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-gray-700">{description}</p>
+      </div>
     </div>
   );
 };
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,51 +1,63 @@
 import React from 'react'
-import image from '../assets/money.jpeg'
 import Card from '../components/Card';
+import image from '../assets/money.jpeg'
+import image2 from '../assets/tech_hardware.jpeg'
+import image3 from '../assets/crypto_minig.jpeg'
+import image4 from '../assets/monitoring.jpeg'
+import image5 from '../assets/mining_plan.jpeg'
+import image6 from '../assets/Data Security.jpeg'
+import image7 from '../assets/mining_setup.jpeg'
 
 const About = () => {
   const cardsData = [
     {
-      title: 'Access the Latest Mining Technology Without Buying Expensive Hardware',
-      description: 'No need to invest in costly equipment—our rental service allows you to access the latest mining hardware and software without the financial burden.',
+      image:image7,
+      title:'No Setup Required – Start Mining Immediately',
+      description:'We handle all the technical details, allowing you to focus on maximizing your earnings without the stress of hardware installation.'
     },
     {
+      image:image3,
+      side:'left',
       title: 'Advanced Cloud Mining Solutions',
       description: 'Harness the power of cloud mining with no upfront costs or long-term commitments.',
     },
     {
-      title: '24/7 Mining Efficiency Monitoring',
-      description: 'Our advanced systems ensure optimal performance round the clock, maximizing your mining output.',
-    },
-    {
+      image:image5,
+      side:'right',
       title: 'Customizable Mining Plans',
       description: 'Tailor your mining plan to your specific needs and budget, ensuring you receive the best value for your money.',
     },
     {
-      title:'24/7 Monitoring and Full Control – Manage Your Mining Anytime, Anywhere',
-      description:'Take control of your mining operations with our intuitive dashboard. Monitor your performance, earnings, and settings from any device, at any time.'
+      image:image4,
+      side:'left',
+      title: '24/7 Mining Efficiency Monitoring',
+      description: 'Our advanced systems ensure optimal performance round the clock, maximizing your mining output.',
     },
     {
+      image:image2,
+      side:'right',
+      title: 'Access the Latest Mining Technology Without Buying Expensive Hardware',
+      description: 'No need to invest in costly equipment—our rental service allows you to access the latest mining hardware and software without the financial burden.',
+    },
+    {
+      image:image6,
       title:'Secure and Reliable: Your Investment, Our Priority',
       description:'We utilize advanced security protocols to safeguard your investments and ensure your data remains confidential. Trust in our commitment to keeping your mining operations safe and sound.'
     },
-    {
-      title:'No Setup Required – Start Mining Immediately',
-      description:'We handle all the technical details, allowing you to focus on maximizing your earnings without the stress of hardware installation.'
-    }
 
     // Add more cards as needed
   ];
   return (
-    <div className='bg-slate-200 min-h-screen mt-0'>
+    <div className='bg-slate-200 min-h-screen mt-0 p-3 '>
 
     <div className='flex pt-2 justify-center'>
-      <img src={image} alt=" money"  className='blur-sm'/>
+      <img src={image} alt=" money"  className='blur-0'/>
     </div>
     <div className=''>
       <h3 className='text-center font-bold m-2 text-[20px]'>Why choose us</h3>
       <div className="flex flex-wrap justify-center">
       {cardsData.map((card, index) => (
-        <Card key={index} title={card.title} description={card.description} />
+        <Card key={index} title={card.title} image={card.image} side={card.side} description={card.description} />
       ))}
     </div>
     </div>
