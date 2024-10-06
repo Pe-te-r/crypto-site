a172fdc92eeabe796bafaf52cc1573c33cd1dcf2
Pe-te-r <mburupeter8526@gmail.com>
2024-10-06 22:58:56 +0300

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -53,20 +53,19 @@ const Home = () => {
   return (
     <div className='min-h-screen mt-10  flex flex-col items-center bg-white-200'>
 
-    <div className='relative' > 
-      <img src={image} alt="" />
+    <div className='relative w-2/4 ' > 
+      <img src={image} alt="" className='w-full h-[650px] blur-sm'/>
 
     <div className='absolute top-2 left-1 bg-yellow-500 p-3 rounded-md'>
       <p>Effortless Crypto Mining-Rent, Mine, Earn</p>
     </div>
     <div className='absolute bottom-1 right-1'>
       <button className='btn-primary bg-blue-500 p-2 font-mono rounded-md'>Explore plans</button>
-      </div>
     </div>
-      <div className='mt-12 border shadow-lg rounded-md p-2 lg:w-2/4 self-center flex flex-col bg-gray-200'>
+    <div className='shadow-lg rounded-md p-2 w-3/4  flex flex-col bg-gray-200 absolute left-0 top-1/3 '>
         <p className='font-bold text-center font-sans'>Enjoy professional-grade mining machines with full maintenance support, optimized for maximum returns.</p>
         <div className='m-2 mb-0 flex items-center place-content-center'>
-          <input type="Email" className='border-2 rounded-md m-1 outline-none p-2 md:w-1/4 sm:w-2/4 font-serif' placeholder='Email'/>
+          <input type="Email" className='border-2 rounded-md m-1 outline-none p-2 w-2/3 font-serif' placeholder='Email'/>
           <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700">
             Login
           </button>
@@ -76,6 +75,7 @@ const Home = () => {
           <p className='font-bold text-center'>Rent powerful mining machines remotely and start earning cryptocurrency without the need for expensive hardware or technical knowledge.</p>
         </div>
       </div>
+    </div>
       <div className='mt-5'>
       <h1 className="text-center text-2xl font-bold mb-6">Rental Packages</h1>
       <div className="flex w-3/4  mx-auto flex-wrap justify-center">
